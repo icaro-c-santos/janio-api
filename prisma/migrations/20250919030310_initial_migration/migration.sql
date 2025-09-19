8ce4ee182b33ce67e2980b402d39c7b20cf6c59b
ICARO SANTOS <icaro.santos@sanar.com>
2025-09-19 00:04:04 -0300

    feat: create all model entities in db

---
+++ prisma/migrations/20250919030310_initial_migration/migration.sql
@@ -0,0 +1,400 @@
+-- CreateEnum
+CREATE TYPE "public"."PhoneType" AS ENUM ('FIXED', 'MOBILE');
+
+-- CreateEnum
+CREATE TYPE "public"."AccountPayableStatus" AS ENUM ('PENDING', 'PAID', 'OVERDUE');
+
+-- CreateEnum
+CREATE TYPE "public"."BillingCycle" AS ENUM ('DAILY', 'MONTHLY', 'WEEKLY');
+
+-- CreateEnum
+CREATE TYPE "public"."InvoiceStatus" AS ENUM ('PENDING', 'PARTIAL', 'PAID', 'OVERDUE', 'CANCELLED');
+
+-- CreateEnum
+CREATE TYPE "public"."AccountReceivableStatus" AS ENUM ('PENDING', 'RECEIVED', 'OVERDUE');
+
+-- CreateEnum
+CREATE TYPE "public"."AccessRole" AS ENUM ('ADMIN', 'SUPPLIER', 'CUSTOMER');
+
+-- CreateEnum
+CREATE TYPE "public"."AccessStatus" AS ENUM ('ACTIVE', 'SUSPENDED');
+
+-- CreateEnum
+CREATE TYPE "public"."ReportStatus" AS ENUM ('PROCESSING', 'PENDING', 'READY', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "public"."UserType" AS ENUM ('INDIVIDUAL', 'COMPANY');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" UUID NOT NULL,
+    "type" "public"."UserType" NOT NULL,
+    "email" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+    "anonymizedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Individual" (
+    "userId" UUID NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "birthDate" TIMESTAMP(3),
+    "deletedAt" TIMESTAMP(3),
+    "anonymizedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Individual_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Company" (
+    "userId" UUID NOT NULL,
+    "cnpj" TEXT NOT NULL,
+    "legalName" TEXT NOT NULL,
+    "tradeName" TEXT,
+    "stateRegistration" TEXT,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Customer" (
+    "userId" UUID NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Supplier" (
+    "userId" UUID NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Address" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "district" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "isPrimary" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Phone" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "areaCode" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "isPrimary" BOOLEAN NOT NULL DEFAULT false,
+    "isWhatsapp" BOOLEAN NOT NULL DEFAULT false,
+    "type" "public"."PhoneType" NOT NULL,
+
+    CONSTRAINT "Phone_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Product" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(10,4) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."CustomerProductPrice" (
+    "id" UUID NOT NULL,
+    "customerId" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "price" DECIMAL(10,4) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CustomerProductPrice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Sale" (
+    "id" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "customerId" UUID NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "unitPrice" DECIMAL(10,4) NOT NULL,
+    "totalPrice" DECIMAL(10,4) NOT NULL,
+    "saleDate" TIMESTAMP(3) NOT NULL,
+    "receiptFileKey" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Sale_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."InventoryWarehouseItem" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "minQuantity" DECIMAL(65,30),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "InventoryWarehouseItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Inventory" (
+    "id" UUID NOT NULL,
+    "itemId" UUID NOT NULL,
+    "quantity" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Inventory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Warehouse" (
+    "id" UUID NOT NULL,
+    "itemId" UUID NOT NULL,
+    "quantity" DECIMAL(65,30) NOT NULL DEFAULT 0,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Warehouse_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."SupplyType" (
+    "id" UUID NOT NULL,
+    "itemId" UUID NOT NULL,
+
+    CONSTRAINT "SupplyType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."SupplyReceipt" (
+    "id" UUID NOT NULL,
+    "supplierId" UUID NOT NULL,
+    "supplyTypeId" UUID NOT NULL,
+    "receivedDate" TIMESTAMP(3) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "unitPrice" DECIMAL(10,4) NOT NULL,
+    "totalPrice" DECIMAL(10,4) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "SupplyReceipt_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AccountPayable" (
+    "id" UUID NOT NULL,
+    "description" TEXT NOT NULL,
+    "totalValue" DECIMAL(10,2) NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "status" "public"."AccountPayableStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+    "expenseId" UUID,
+    "supplyReceiptId" UUID,
+    "paymentDate" TIMESTAMP(3),
+    "paymentReceiptFileKey" TEXT,
+
+    CONSTRAINT "AccountPayable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ExpenseType" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "ExpenseType_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Expense" (
+    "id" UUID NOT NULL,
+    "expenseTypeId" UUID NOT NULL,
+    "description" TEXT NOT NULL,
+    "totalValue" DECIMAL(10,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "receiptFileKey" TEXT,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."BillingPlan" (
+    "id" UUID NOT NULL,
+    "customerId" UUID NOT NULL,
+    "cycle" "public"."BillingCycle" NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3),
+    "dueDay" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "BillingPlan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Invoice" (
+    "id" UUID NOT NULL,
+    "periodStart" TIMESTAMP(3) NOT NULL,
+    "periodEnd" TIMESTAMP(3) NOT NULL,
+    "description" TEXT,
+    "totalValue" DECIMAL(10,2) NOT NULL,
+    "dueDate" TIMESTAMP(3) NOT NULL,
+    "status" "public"."InvoiceStatus" NOT NULL DEFAULT 'PENDING',
+    "metadata" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Invoice_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."AccountReceivable" (
+    "id" UUID NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "status" "public"."AccountReceivableStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "invoiceId" UUID NOT NULL,
+
+    CONSTRAINT "AccountReceivable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Access" (
+    "id" UUID NOT NULL,
+    "userId" UUID NOT NULL,
+    "role" "public"."AccessRole" NOT NULL,
+    "status" "public"."AccessStatus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Access_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Report" (
+    "id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "type" TEXT NOT NULL,
+    "status" "public"."ReportStatus" NOT NULL DEFAULT 'PENDING',
+    "fileKey" TEXT,
+    "requestedBy" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Individual_cpf_key" ON "public"."Individual"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Company_cnpj_key" ON "public"."Company"("cnpj");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CustomerProductPrice_customerId_productId_key" ON "public"."CustomerProductPrice"("customerId", "productId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ExpenseType_name_key" ON "public"."ExpenseType"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Access_userId_role_key" ON "public"."Access"("userId", "role");
+
+-- AddForeignKey
+ALTER TABLE "public"."Individual" ADD CONSTRAINT "Individual_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Company" ADD CONSTRAINT "Company_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Customer" ADD CONSTRAINT "Customer_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Supplier" ADD CONSTRAINT "Supplier_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Phone" ADD CONSTRAINT "Phone_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CustomerProductPrice" ADD CONSTRAINT "CustomerProductPrice_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "public"."Customer"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."CustomerProductPrice" ADD CONSTRAINT "CustomerProductPrice_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Sale" ADD CONSTRAINT "Sale_productId_fkey" FOREIGN KEY ("productId") REFERENCES "public"."Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Sale" ADD CONSTRAINT "Sale_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "public"."Customer"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Inventory" ADD CONSTRAINT "Inventory_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "public"."InventoryWarehouseItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Warehouse" ADD CONSTRAINT "Warehouse_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "public"."InventoryWarehouseItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."SupplyType" ADD CONSTRAINT "SupplyType_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "public"."InventoryWarehouseItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."SupplyReceipt" ADD CONSTRAINT "SupplyReceipt_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "public"."Supplier"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."SupplyReceipt" ADD CONSTRAINT "SupplyReceipt_supplyTypeId_fkey" FOREIGN KEY ("supplyTypeId") REFERENCES "public"."SupplyType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."AccountPayable" ADD CONSTRAINT "AccountPayable_expenseId_fkey" FOREIGN KEY ("expenseId") REFERENCES "public"."Expense"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."AccountPayable" ADD CONSTRAINT "AccountPayable_supplyReceiptId_fkey" FOREIGN KEY ("supplyReceiptId") REFERENCES "public"."SupplyReceipt"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Expense" ADD CONSTRAINT "Expense_expenseTypeId_fkey" FOREIGN KEY ("expenseTypeId") REFERENCES "public"."ExpenseType"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."BillingPlan" ADD CONSTRAINT "BillingPlan_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "public"."Customer"("userId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."AccountReceivable" ADD CONSTRAINT "AccountReceivable_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "public"."Invoice"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Access" ADD CONSTRAINT "Access_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
